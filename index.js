25f10fd66af9b9c3d38f4a786040cb1e0d9228c4
BGM-007 <162390742+BGM-007@users.noreply.github.com>
2025-01-15 11:03:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function hi()
+{
+    let a,b;
+    a=5;
+    b=10;
+    var c=a+b;
+    document.getElementById('ADD').innerHTML=c;
+    alert('hello');
+}
+
+function cl()
+{
+    document.getElementById('ADD').innerHTML=null;
+}
